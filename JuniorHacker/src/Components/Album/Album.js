bfec25f51a11aa8eb670a9da5dd30b3df1aeee18
Dareen Znbarakji <64015002+DareenZnbarakji@users.noreply.github.com>
2020-07-06 15:02:23 +0200

    changes

--- JuniorHacker/src/Components/Album/Album.js
+++ JuniorHacker/src/Components/Album/Album.js
@@ -1,93 +1,186 @@
 import React from 'react'
 import Navigation from "../Common/Navigation/Navigation"
-import Carousel from 'react-elastic-carousel';
-import Item from "./Item";
-import "./styles.css";
+import Carousel from 'react-bootstrap/Carousel'
+import {Container, Row, Col} from 'react-bootstrap'
 import album1 from "../../Images/album1.jpg"
 import album2 from "../../Images/album2.jpg"
 import album3 from "../../Images/album3.jpg"
 import album4 from "../../Images/album4.jpg"
 import album5 from "../../Images/album5.jpg"
-
-
-
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 1, itemsToShow: 2 },
-  { width: 1, itemsToShow: 3 },
-  { width: 1, itemsToShow: 4 }
-];
+import album6 from "../../Images/album6.jpg"
 
 
 function Album() {
   return (
+    <div>
+  <Container>
 
-    <div className='style'>
-      <Navigation />
-        <div class="ui segment">
-         <p>Album 2016</p>
-        </div>
-        <Carousel className="Album" breakPoints={breakPoints}>
-         <Item><img src={album1} alt="{image}" className='image'/></Item>
-        <Item><img src={album2} alt="{image}" className='image'/></Item>
-        <Item><img src={album3} alt="{image}" className='image'/></Item>
-        <Item><img src={album4} alt="{image}" className='image'/></Item>
-        <Item><img src={album5} alt="{image}" className='image'/></Item>
+  <Row>
+    <Col>
+    <Carousel className="d-block w-50px">
+      <Carousel.Item >
+        <img
+          className="d-block w-100"
+          src={album1}
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={album2}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={album3}
+          alt="Third slide"
+        />
+
+      </Carousel.Item>
+    </Carousel>
+    </Col>
+    <Col><h1 style={{fontFamily:'Roman', color:'black'}}>Album 2016</h1>
+    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></Col>
+    <Col><h1 style={{fontFamily:'Roman', color:'black'}}>Memories</h1>
+    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.
+    Nulla vitae elit libero, a pharetra augue mollis interdum.
+    Nulla vitae elit libero, a pharetra augue mollis interdum
+    Nulla vitae elit libero, a pharetra augue mollis interdum</p>
+    </Col>
+  </Row>
+</Container>
+    <br/><br/>
+    <Container>
+
+    <Row>
+
+    <Col><h1 style={{fontFamily:'Roman', color:'black'}}>Memories</h1>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.
+      Nulla vitae elit libero, a pharetra augue mollis interdum.
+      Nulla vitae elit libero, a pharetra augue mollis interdum
+      Nulla vitae elit libero, a pharetra augue mollis interdum</p>
+      </Col>
+     
+      <Col><h1 style={{fontFamily:'Roman', color:'black'}}>Album 2017</h1>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></Col>
+      <Col>
+      <Carousel className="d-block w-50px">
+        <Carousel.Item >
+          <img
+            className="d-block w-100"
+            src={album4}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={album5}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={album6}
+            alt="Third slide"
+          />
+
+        </Carousel.Item>
       </Carousel>
-      
-        
+      </Col>
+    </Row>
+    </Container>
+    <br/><br/>
+    <Container>
+
+  <Row>
+    <Col>
+    <Carousel className="d-block w-50px">
+      <Carousel.Item >
+        <img
+          className="d-block w-100"
+          src={album1}
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={album2}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={album3}
+          alt="Third slide"
+        />
+
+      </Carousel.Item>
+    </Carousel>
+    </Col>
+    <Col><h1 style={{fontFamily:'Roman', color:'black'}}>Album 2018</h1>
+    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></Col>
+    <Col><h1 style={{fontFamily:'Roman', color:'black'}}>Memories</h1>
+    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.
+    Nulla vitae elit libero, a pharetra augue mollis interdum.
+    Nulla vitae elit libero, a pharetra augue mollis interdum
+    Nulla vitae elit libero, a pharetra augue mollis interdum</p>
+    </Col>
+  </Row>
+</Container>
+    <br/><br/>
+    <Container>
+
+    <Row>
+
+    <Col><h1 style={{fontFamily:'Roman', color:'black'}}>Memories</h1>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.
+      Nulla vitae elit libero, a pharetra augue mollis interdum.
+      Nulla vitae elit libero, a pharetra augue mollis interdum
+      Nulla vitae elit libero, a pharetra augue mollis interdum</p>
+      </Col>
+     
+      <Col><h1 style={{fontFamily:'Roman', color:'black'}}>Album 2019</h1>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p></Col>
+      <Col>
+      <Carousel className="d-block w-50px">
+        <Carousel.Item >
+          <img
+            className="d-block w-100"
+            src={album4}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={album5}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={album6}
+            alt="Third slide"
+          />
+
+        </Carousel.Item>
+      </Carousel>
+      </Col>
+    </Row>
+    </Container>
      
     </div>
   )
 }
 
-export default Album
 
-
-// import React from 'react'
-// import Carousel from 'react-bootstrap/Carousel'
-// export default function Album() {
-//   return (
-//     <div>
-//       <Carousel>
-//   <Carousel.Item>
-//     <img
-//       className="d-block w-100"
-//       src="holder.js/800x400?text=First slide&bg=373940"
-//       alt="First slide"
-//     />
-//     <Carousel.Caption>
-//       <h3>First slide label</h3>
-//       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-//     </Carousel.Caption>
-//   </Carousel.Item>
-//   <Carousel.Item>
-//     <img
-//       className="d-block w-100"
-//       src="holder.js/800x400?text=Second slide&bg=282c34"
-//       alt="Third slide"
-//     />
-
-//     <Carousel.Caption>
-//       <h3>Second slide label</h3>
-//       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-//     </Carousel.Caption>
-//   </Carousel.Item>
-//   <Carousel.Item>
-//     <img
-//       className="d-block w-100"
-//       src="holder.js/800x400?text=Third slide&bg=20232a"
-//       alt="Third slide"
-//     />
-
-//     <Carousel.Caption>
-//       <h3>Third slide label</h3>
-//       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-//     </Carousel.Caption>
-//   </Carousel.Item>
-// </Carousel>
-//     </div>
-//   )
-// }
-
+export default Album;
  
