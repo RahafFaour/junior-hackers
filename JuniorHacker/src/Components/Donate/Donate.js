7da06122ac24e4b1a7f4b50cad87ead9c78c0a6c
Dareen Znbarakji <64015002+DareenZnbarakji@users.noreply.github.com>
2020-07-08 20:51:43 +0200

    finished Donate component

--- JuniorHacker/src/Components/Donate/Donate.js
+++ JuniorHacker/src/Components/Donate/Donate.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import Navigation from "../Common/Navigation/Navigation"
 import Footer from "../Common/Footer/Footer"
 import "./styles.css";
+import {Media,Container, Col,Row} from 'react-bootstrap'
+import imageDon from './imageDon.jpg'
+import imageDona from './imageDona.jpg'
+import Artikel from './Artikel.pdf'
+
+
+
 const storeItems = [
   {
     id: "order-1",
@@ -72,20 +79,27 @@ async function buyItem(item) {
 }
 
 function Donate() {
+
   const [items] = useState(storeItems);
   
   return (
-    <React.Fragment>      <Navigation/>
-    <div className="Donate">
-      <h1>Rondo The Hairdresser's store</h1>
+    <Container>
+      <Navigation/>
+      <br/><br/>
+  <Row>
+    <Col xs={6} md={6}>
+    <React.Fragment>  
+          
+    <div className="Donate" border="success" style={{ width: '38rem', marginLeft: '0' }}>
+      <h1>U heeft dit werk met uw bijdrage aan Yoreem mede mogelijk gemaakt. Namens alle betrokkenen zeggen wij u daarvoor </h1>
       <div>
-        <h2>Items for sale</h2>
+        <h2>heel hartelijk dank!</h2>
         <ul className="items">
           {items.map((item, index) => (
             <li>
               <p className="items__label">{item.label}</p>
               <span>${item.amount}</span>
-              <button
+              <button 
                 className="items__button"
                 onClick={() => buyItem(item)}
                 type="button">
@@ -97,12 +111,68 @@ function Donate() {
       </div>
       
     </div>
-    <Footer/>
+  
 
     </React.Fragment>
+    </Col>
+    <Col xs={12} md={6}>
+              <Media>
+          <img
+            width={64}
+            height={64}
+            className="align-self-start mr-3"
+            src={imageDona}
+            alt="Generic placeholder"
+          />
+          <Media.Body>
+            <h5 style={{fontFamily:'Roman', color:'black', fontWeight: 'bold'}}>Mevrouw Sonia Puma (38)</h5>
+            <p style={{fontFamily:'Roman'}}>
+            heeft een handicap, ze loopt al vijf jaar met een
+                rollator. In 2015 bood een NGO uit Cusco aan om haar te helpen met een
+                heupoperatie. Helaas ging de operatie mis, waardoor haar rechterbeen
+                onbruikbaar werd.
+                <a href={Artikel} className="ui positive button" rel="noopener noreferrer" target="_blank"
+             style={{fontFamily:'Roman', backgroundColor:'white', fontWeight: 'bold', color:'black'}}>More...</a>
+            </p>
+
+          </Media.Body>
+          </Media>
+
+          <Media>
+          <img
+            width={64}
+            height={64}
+            className="align-self-center mr-3"
+            src={imageDon}
+            alt="Generic placeholder"
+          />
+          <Media.Body>
+            <h5 style={{fontFamily:'Roman', color:'black', fontWeight: 'bold'}}>Mevrouw Verónica Ccana (45)</h5>
+            <p style={{fontFamily:'Roman'}}>
+            en mevrouwLorenza Choquetocro(39) zijn buren. Zewonen allebei indezelfde str aat. Beidevrouwen zijn
+                      alleenstaande moeders. De eerste moeder woont samen met haar negenjarige dochter en de tweede moeder woont met
+                      haar drie kinder 
+                      <a href={Artikel} className="ui positive button" rel="noopener noreferrer" target="_blank"
+             style={{fontFamily:'Roman', backgroundColor:'white', fontWeight: 'bold', color:'black'}}>More...</a>
+            </p>
+
+         
+          </Media.Body>
+          </Media>
+            <br/><br/>
+
+     </Col>
+     </Row>
+      <Footer/>
+    </Container>
+
+
+    
 
   );
 }
 
 
 export default Donate;
+
+
